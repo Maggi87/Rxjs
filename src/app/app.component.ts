7cf71e03d86bb867a2395593b364c74d13f933c0
Dattaprasad Ingle <mauli.ingle@gmail.com>
2023-12-28 21:20:21 +0530

    create operation of, from

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {of, from} from "rxjs"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs';
+
+  ngOnInit():void{
+    of(2,4,6).subscribe(x=>console.log(x));
+
+    from([20,15,10]).subscribe(
+      next => console.log(`resulting item... ${next}`),
+      error => console.log(`error occured ${error}`),
+      () => console.log('done')
+    );
+  }
+
 }
